ba0a41514e97a72a73db06682f70e9443005dbb0
kate-del <chernyakova.c@gmail.com>
2022-03-23 14:49:41 +0000

    fix tests

--- __tests__/visits.test.js
+++ __tests__/visits.test.js
@@ -8,10 +8,10 @@ const saveTestData = require("../backend/config/seed-visits.js");
 const connectDB = require("../backend/config/db.js");
 // const { beforeEach, expect } = require("@jest/globals");
 let data;
-beforeEach(() => {
+beforeEach(async () => {
   //   connectDB();
 
-  mongoose.connection
+  await mongoose.connection
     .dropDatabase()
     .then(saveTestData)
     .then((savedData) => (data = savedData))
@@ -25,7 +25,7 @@ afterAll(() => {
 describe("GET /api/users/", () => {
   test("GET returns an object with all users", () => {
     return request(app)
-      .get("/api/users/")
+      .get("/api/users?userRole=volunteer")
       .expect(200)
       .then(({ body: { users } }) => {
         console.log(users);
--- backend/config/seed-visits.js
+++ backend/config/seed-visits.js
@@ -4,7 +4,6 @@ const dotenv = require("dotenv").config();
 const colors = require("colors");
 const Visit = require("../models/visitModel");
 const User = require("../models/userModel");
-// connectDB();
 const savedData = {};
 
 const saveUser = () => {
@@ -26,57 +25,30 @@ const saveUser = () => {
       userRole: "visitee",
     },
   ];
-  //   .map((user) => User.create(user));
+
   return User.insertMany(users);
-  //   return Promise.all(users);
 };
 
 const saveVisit = (users) => {
-  const visit = new Visit({
-    volunteerId: users[0]._id,
-    volunteerFirstName: users[0].firstName,
-    volunteerLastName: users[0].lastName,
-    visiteeId: users[1]._id,
-    visiteeFirstName: users[1].firstName,
-    visiteeLastName: users[1].lastName,
-  });
-  return visit.save();
+  const visits = [
+    {
+      volunteerId: users[0]._id,
+      volunteerFirstName: users[0].firstName,
+      volunteerLastName: users[0].lastName,
+      visiteeId: users[1]._id,
+      visiteeFirstName: users[1].firstName,
+      visiteeLastName: users[1].lastName,
+    },
+  ];
+  return Visit.insertMany(visits);
 };
 
-const saveTestData = () => {
-  //   connectDB();
-  //   console.log(mongoose.connection.collections.visits, "connection");
-  //   console.log(mongoose.c, "collection");
-  //   console.log(mongo.c, "collection");
-  //   return (
-  // Promise.all([
-  //   //   await connectDB(),
-  //   User.deleteMany({}),
-  //   //   mongoose.connection.dropCollection(),
-  //   //   Visit.collection.deleteMany({}),
-  // ])
-  // Promise.all([
-  //   mongoose.connection.createCollection("users"),
-  //   mongoose.connection.createCollection("visits"),
-  // ])
-
-  // return Promise.all([User.init(), Visit.init()]);
-
-  // .then(() => {
-  //   return Promise.all([User.init(), Visit.init()]);
-  // })
-  //   .then(() => {
-  return saveUser()
-    .then((users) => {
-      savedData.user = users;
-      return saveVisit(users);
-    })
-    .then((visit) => {
-      savedData.visits = visit;
-      return savedData;
-    });
-  //   })
-  //   );
+const saveTestData = async () => {
+  const users = await saveUser();
+  savedData.users = users;
+  const visits = await saveVisit(users);
+  savedData.visits = visits;
+  return savedData;
 };
 
 module.exports = saveTestData;

2e0fc293bed81bd8dffdfaac7bcd3b03291ccf77
kate-del <chernyakova.c@gmail.com>
2022-03-24 13:54:24 +0000

    send error message when no visit found

--- backend/controllers/visitController.js
+++ backend/controllers/visitController.js
@@ -16,25 +16,26 @@ const getAllVisits = asyncHandler(async (req, res, next) => {
 });
 
 const getVisitByVisitId = asyncHandler(async (req, res, next) => {
-    try {
-      const visit = await Visit.findById(req.params.visitId);
-        const visitArr = [...visit]
-console.log(visitArr)
-      if (!visitArr.length) {
-        res.status(404);
-        throw new Error("No visit found");
-      }
-      res.status(200).json(visit);
-    } catch (error) {
-      next(error);
-    }
-  });
+  const visitId = req.params.visitId;
+  if (visitId.length !== 24) {
+    res.status(404);
+    throw new Error("Invalid id");
+  }
 
-
+  await Visit.findById(visitId)
+    .exec()
+    .then((visit) => {
+      res.status(200).send(visit);
+    })
+    .catch((error) => {
+      res.status(404);
+      throw new Error("No visit found");
+    });
+});
 
 module.exports = {
   getAllVisits,
-  getVisitByVisitId
+  getVisitByVisitId,
   //   setUser,
   // updateUser,
   // deleteUser,
--- backend/server.js
+++ backend/server.js
@@ -3,25 +3,26 @@ const dotenv = require("dotenv").config();
 //const port = process.env.PORT || 9000;
 const app = express();
 const colors = require("colors");
-const { errorHandler } = require("./middleware/errorMiddleware");
+const {
+  errorHandler,
+  handleCustomErrors,
+} = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-const port = Math.floor(Math.random()*10000)
+const port = Math.floor(Math.random() * 10000);
 
 connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/visits", require("./routes/visitsRoutes"));
 
-app.use('/api/users', require('./routes/userRoutes'))
-app.use('/api/visits', require('./routes/visitsRoutes'))
-
-
+app.use(handleCustomErrors);
 app.use(errorHandler);
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
-
-app.all('/*', (req, res) => {
-    res.status(404).send({message: "Path not found"})
-})
+app.all("/*", (req, res) => {
+  res.status(404).send({ message: "Path not found" });
+});
 module.exports = app;

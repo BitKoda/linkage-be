de8d7c40706747c46082a6480b98db59ba10de9c
kate-del <chernyakova.c@gmail.com>
2022-03-30 17:46:08 +0100

    update avatar url

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -17,7 +17,7 @@ const userSchema = mongoose.Schema(
     avatar_url: {
       type: String,
       default:
-        "https://kcl-mrcdtp.com/wp-content/uploads/sites/201/2021/03/No-Image-1.png",
+        "https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1214428300?k=20&m=1214428300&s=170667a&w=0&h=NPyJe8rXdOnLZDSSCdLvLWOtIeC9HjbWFIx8wg5nIks=",
       required: false,
     },
     interests: {
